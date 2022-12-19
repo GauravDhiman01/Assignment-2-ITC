4816b384de19423b226cd5d6674b44e583f55c8d
GauravDhiman01 <121049750+GauravDhiman01@users.noreply.github.com>
2022-12-19 23:17:19 +0530

    Add files via upload

---
+++ Q2.py
@@ -0,0 +1,6 @@
+Name=str(input("Enter your Name : "))
+SID=int(input("Enter your SID : "))
+Department=str(input("Department : "))
+CGPA= 9.5
+
+print("\n\nHey, %s Here! \nMy SID is %d\nI am from %s department and my CGPA is %f " % (Name,SID,Department,CGPA))
